4f9d11cd92ba54941fd7e574d1725b91d0fcebd3
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-08-12 11:23:39 +0300

    line truncation (clamping)

--- src/components/MainStory/MainStory.js
+++ src/components/MainStory/MainStory.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import { QUERIES } from "../../constants";
+
+
 const MainStory = ({
   id,
   title,
@@ -44,6 +47,22 @@ const Abstract = styled.p`
   font-size: 1rem;
   margin-bottom: 1em;
   white-space: pre-wrap;
+
+  /* CLAMP TEXT */
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 8;
+  overflow: hidden;
+  /* ------ */
+
+  @media ${QUERIES.tabletAndUp} {
+    -webkit-line-clamp: 16;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    -webkit-line-clamp: 10;
+  }
+
 `;
 
 const Location = styled.span`
